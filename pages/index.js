c689d93e679d4768f8fadadc86a2766f213816b6
highfee <odediranifeoluwa7@gmail.com>
2022-12-07 16:14:27 +0100

    asd

--- pages/index.js
+++ pages/index.js
@@ -44,6 +44,7 @@ export default function Home({ data }) {
 export const getServerSideProps = async () => {
   const res = await axios.get("https://khemsafe.vercel.app/api/products");
   // const res = await axios.get("http://localhost:3000/api/products");
+  console.log(res);
 
   return {
     props: {
