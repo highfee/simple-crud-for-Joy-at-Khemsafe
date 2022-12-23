1e2223eeff4f00c0f90d1c9d4a0df0a29efbd7b9
highfee <odediranifeoluwa7@gmail.com>
2022-12-23 12:09:29 +0100

    added a delete icon to products

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -6,8 +6,8 @@ import { FaTrashAlt } from "react-icons/fa";
 const ProductCard = ({ product }) => {
   const [deleteProduct, setDeleteProduct] = useState(false);
   return (
-    <Link href={`/${product._id}`} passHref>
-      <div className="p-3 bg-white rounded-md shadow-sm shadow-gray-300 mb-4 flex relative">
+    <div className="p-3 bg-white rounded-md shadow-sm shadow-gray-300 mb-4 flex relative">
+      <Link href={`/${product._id}`} passHref>
         <div>
           <h2>{product.name}</h2>
           <p>
@@ -24,24 +24,24 @@ const ProductCard = ({ product }) => {
             Quantity remaining: <span>{product.quantity}</span>
           </p>
         </div>
-        {deleteProduct ? (
-          <div className="absolute flex gap-2 bottom-3 right-3">
-            <button className="border-2 border-solid px-3 py-1">Ok</button>
-            <button
-              onClick={(prev) => setDeleteProduct(false)}
-              className="border-2 border-solid px-3 py-1"
-            >
-              Cancel
-            </button>
-          </div>
-        ) : (
-          <FaTrashAlt
-            className="absolute bottom-3 right-3 cursor-pointer"
-            onClick={(prev) => setDeleteProduct(true)}
-          />
-        )}
-      </div>
-    </Link>
+      </Link>
+      {deleteProduct ? (
+        <div className="absolute flex gap-2 bottom-3 right-3">
+          <button className="border-2 border-solid px-3 py-1">Ok</button>
+          <button
+            onClick={(prev) => setDeleteProduct(false)}
+            className="border-2 border-solid px-3 py-1"
+          >
+            Cancel
+          </button>
+        </div>
+      ) : (
+        <FaTrashAlt
+          className="absolute bottom-3 right-3 cursor-pointer"
+          onClick={(prev) => setDeleteProduct(true)}
+        />
+      )}
+    </div>
   );
 };
 
