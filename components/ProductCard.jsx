c9a8226baab25b4ccbba8ec5d02342142abbc0ce
highfee <odediranifeoluwa7@gmail.com>
2022-12-23 12:05:11 +0100

    added a delete icon to products

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import { useState } from "react";
 import CurrencyFormat from "react-currency-format";
 import { FaTrashAlt } from "react-icons/fa";
 
 const ProductCard = ({ product }) => {
+  const [deleteProduct, setDeleteProduct] = useState(false);
   return (
     <Link href={`/${product._id}`} passHref>
       <div className="p-3 bg-white rounded-md shadow-sm shadow-gray-300 mb-4 flex relative">
@@ -22,7 +24,22 @@ const ProductCard = ({ product }) => {
             Quantity remaining: <span>{product.quantity}</span>
           </p>
         </div>
-        <FaTrashAlt className="absolute bottom-3 right-3" />
+        {deleteProduct ? (
+          <div className="absolute flex gap-2 bottom-3 right-3">
+            <button className="border-2 border-solid px-3 py-1">Ok</button>
+            <button
+              onClick={(prev) => setDeleteProduct(false)}
+              className="border-2 border-solid px-3 py-1"
+            >
+              Cancel
+            </button>
+          </div>
+        ) : (
+          <FaTrashAlt
+            className="absolute bottom-3 right-3 cursor-pointer"
+            onClick={(prev) => setDeleteProduct(false)}
+          />
+        )}
       </div>
     </Link>
   );
