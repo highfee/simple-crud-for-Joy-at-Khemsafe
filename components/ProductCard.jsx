6dad7a075ae747ac5c5fd20fb034c0cee102980f
highfee <odediranifeoluwa7@gmail.com>
2022-12-23 11:54:34 +0100

    added a delete icon to products

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import CurrencyFormat from "react-currency-format";
+import { FaTrashAlt } from "react-icons/fa";
 
 const ProductCard = ({ product }) => {
   return (
     <Link href={`/${product._id}`} passHref>
-      <div className="p-3 bg-white rounded-md shadow-sm shadow-gray-300 mb-4 flex">
+      <div className="p-3 bg-white rounded-md shadow-sm shadow-gray-300 mb-4 flex relative">
         <div>
           <h2>{product.name}</h2>
           <p>
@@ -21,15 +22,7 @@ const ProductCard = ({ product }) => {
             Quantity remaining: <span>{product.quantity}</span>
           </p>
         </div>
-        {/* <div>
-        <h2>{product.name}</h2>
-        <p>
-        Price: <span>{product.price}</span>
-        </p>
-        <p>
-        Quantity remaining: <span>{product.quantity}</span>
-        </p>
-    </div> */}
+        <FaTrashAlt className="absolute top-3 right-3" />
       </div>
     </Link>
   );
